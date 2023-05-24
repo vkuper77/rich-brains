b445183e912922a5963505d34699be3bf184b61a
Vitali <vkuper7@gmail.com>
2023-05-24 21:02:31 +0300

    feat: add api app

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,36 @@
+import {instance} from "./instance";
+
+export class AuthApi {
+    static async signIn({login, password}: { login: string, password: string }) {
+        const response = await instance.post(`user/login`, {login, password})
+        instance.setTokenToLocalStorage('')
+        return response
+    }
+
+    static signOut() {
+        instance.removeTokenFromLocalStorage()
+    }
+}
+
+export class AppApi {
+
+    static async getClients() {
+        return await instance.get(`clients`)
+    }
+
+    static async getClient({id}: { id: number }) {
+        return await instance.post(`clients/get`, {id})
+    }
+
+    static async addClient({name, surname, age, phone}: { name: string, surname: string, age: number, phone: string }) {
+        return await instance.post(`clients/add`, {name, surname, age, phone})
+    }
+
+    static async editClient({name, surname, age, phone, id}: { name: string, surname: string, age: number, phone: string, id: number }) {
+        return await instance.put(`clients/edit`, {name, surname, age, phone, id})
+    }
+
+    static async deleteClient({id}: { id: number }) {
+        return await instance.delete(`clients/remove?id=${id}`)
+    }
+}
---
+++ frontend/src/modules/fetch.ts
@@ -0,0 +1,76 @@
+export class FetchConstructor {
+    private readonly baseUrl: string;
+
+    constructor(url: string) {
+        this.baseUrl = url;
+    }
+
+    async get(route: string): Promise<any> {
+        const url = this.baseUrl + route;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: this.getHeaders(),
+        });
+        return this.handleResponse(response);
+    }
+
+    async post(route: string, body: any): Promise<any> {
+        const url = this.baseUrl + route;
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: this.getHeaders(),
+            body: JSON.stringify(body),
+        });
+        return this.handleResponse(response);
+    }
+
+    async put(route: string, body: any): Promise<any> {
+        const url = this.baseUrl + route;
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: this.getHeaders(),
+            body: JSON.stringify(body),
+        });
+        return this.handleResponse(response);
+    }
+
+    async delete(route: string): Promise<any> {
+        const url = this.baseUrl + route;
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: this.getHeaders(),
+        });
+        return this.handleResponse(response);
+    }
+
+    private getHeaders(): Record<string, string> {
+        const headers: Record<string, string> = {
+            'Content-Type': 'application/json',
+        };
+        const token = this.getTokenFromLocalStorage();
+        if (token) {
+            headers['Authorization'] = `${token}`;
+        }
+        return headers;
+    }
+
+    private getTokenFromLocalStorage(): string | null {
+        return localStorage.getItem('token');
+    }
+
+    setTokenToLocalStorage(token: string): void {
+        localStorage.setItem('token', token);
+    }
+
+    removeTokenFromLocalStorage(): void {
+        localStorage.removeItem('token');
+    }
+
+    private async handleResponse(response: Response): Promise<any> {
+        if (!response.ok) {
+            throw new Error('Request failed');
+        }
+        return response.json();
+    }
+}
+
